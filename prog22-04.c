f6d15253a3afa5e6f2a44b4a44c5bae0b801627f
ryuutafuruhashi310@gmail.com <ei2031@hamako-ths.ed.jp>
2022-01-26 18:30:44 +0900

    prog22

---
+++ prog22-04.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <hamakou.h>
+#include "stack.h"
+
+main()
+{
+  Stack x;
+  int data1, data2,state;
+
+  initStack(&x);                   // スタックの初期化
+
+  data1 = getint("push data1 : "); // データ１を入力
+  data2 = getint("push data2 : "); // データ２を入力
+
+  printf("\n交換前 - data1: %d, data2: %d\n", data1, data2);
+
+                 ;  // 入力データ１と２をスタックへ格納　
+                 ;  // (データ格納後のスタックの状態を意識しておくこと)
+  state = push(&x,data1);
+  state = push(&x,data2);
+                 ;  // データ１と２をスタックから取り出す
+                 ;  // (スタックの状態からデータの取り出し順序を考える)
+  state = pop(&x,&data1);
+  state = pop(&x,&data2);
+  printf("交換後 - data1: %d, data2: %d\n\n", data1, data2);
+
+  return(0);
+}
